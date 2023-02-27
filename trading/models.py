5b683eec1b2ea228c987f175c876835c304a4ebd
NikGorbunov <gorbunovmya.@gmail.com>
2023-02-27 10:08:45 +0200

    create user modal

--- trading/models.py
+++ trading/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class UserSetting(models.Model):
+    phone_number = models.CharField(max_length=20)
+    coin = models.CharField(max_length=10)
+    high_price = models.DecimalField(max_digits=10, decimal_places=2)
+    low_price = models.DecimalField(max_digits=10, decimal_places=2)
