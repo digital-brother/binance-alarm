e391b9f2ee12ced285d93ae2c30cff4309ca0577
NikGorbunov <gorbunovmya.@gmail.com>
2023-03-17 17:57:45 +0200

    was created for the phone number added several thresholds for triggering alarms using the built-in forms in the admin panel

--- trading/admin.py
+++ trading/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 
 from trading.models import Coin, PhoneNumber
 
-admin.site.register(PhoneNumber)
+
+class ThresholdInline(admin.TabularInline):
+    model = Coin
+    extra = 1
+
+
+class PhoneNumberAdmin(admin.ModelAdmin):
+    inlines = [ThresholdInline]
+
+
+admin.site.register(PhoneNumber, PhoneNumberAdmin)
 
 admin.site.register(Coin)
