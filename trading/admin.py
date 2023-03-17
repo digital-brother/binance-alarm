65a47757369bd2359a125038446dcf13e29dc965
NikGorbunov <gorbunovmya.@gmail.com>
2023-03-17 17:12:45 +0200

    was created models coin, phone number, and implemented a custom user model to register user with phone numbers

--- trading/admin.py
+++ trading/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from trading.models import Coin, PhoneNumber
+
+admin.site.register(PhoneNumber)
+
+admin.site.register(Coin)
--- trading/models.py
+++ trading/models.py
@@ -1,3 +1,46 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class PhoneNumberManager(BaseUserManager):
+    def create_user(self, phone_number, password=None):
+        if not phone_number:
+            raise ValueError('Users must have a phone number')
+
+        user = self.model(phone_number=phone_number)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone_number, password):
+        user = self.create_user(phone_number=phone_number, password=password)
+        user.is_admin = True
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class PhoneNumber(AbstractBaseUser, PermissionsMixin):
+    phone_number = PhoneNumberField(blank=True, unique=True, region='UA')
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    pause_service = models.BooleanField(default=False)
+
+    objects = PhoneNumberManager()
+
+    USERNAME_FIELD = 'phone_number'
+
+    def __str__(self):
+        return str(self.phone_number)
+
+
+class Coin(models.Model):
+    phone_number = models.ForeignKey(PhoneNumber, on_delete=models.CASCADE)
+    coin = models.CharField(max_length=255)
+    threshold = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return str(self.coin)
