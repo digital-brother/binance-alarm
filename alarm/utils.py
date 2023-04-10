d883e069afbd0e6cf45562a8880b0553e8c4e869
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-04-10 19:43:09 +0300

    Fix alarm/utils.py

--- alarm/utils.py
+++ alarm/utils.py
@@ -1,52 +1,23 @@
-from alarm.binance_utils import parse_kindle_data_from_binance_websocket_update, print_binance_candle_data
-from alarm.models import Threshold
+from alarm.models import Threshold, Candle
 
 
-def update_coin_candle_from_binance_data(data):
-    try:
-        # Parse the message to extract relevant data
-        current_candle_high_price, current_candle_low_price, trade_pair = \
-            parse_kindle_data_from_binance_websocket_update(data)
-
-        threshold = Threshold.objects.filter(abbreviation=trade_pair).first()
-
-        if not threshold:
-            # There is no coin with this trade_pair in the database
-            # TODO: Show error with Sentry
-            return
-
-        threshold.update_or_create_candles(current_candle_high_price, current_candle_low_price)
-
-        threshold = threshold.price
-
-        last_candle_high_price = threshold.last_high_price
-        last_candle_low_price = threshold.last_low_price
-
-        print_binance_candle_data(trade_pair, current_candle_high_price, threshold,
-                                  current_candle_low_price)
-        coin_prices_data = {
-            'current_candle_high_price': current_candle_high_price,
-            'current_candle_low_price': current_candle_low_price,
-            'threshold': threshold,
-            'last_candle_high_price': last_candle_high_price,
-            'last_candle_low_price': last_candle_low_price
-        }
-
-        return coin_prices_data
-
-    except (KeyError, ValueError, TypeError) as e:
-        # An error occurred while parsing the message
-        # TODO: Show error with Sentry
-        pass
+def threshold_is_broken(threshold_price, previous_candle, current_candle):
+    if previous_candle:
+        return (
+                min(previous_candle.low_price, current_candle.low_price) <=
+                threshold_price <=
+                max(previous_candle.high_price, current_candle.high_price)
+        )
+    return False
 
 
-def threshold_is_broken(coin_prices_data):
-    if coin_prices_data:
-        threshold = coin_prices_data['threshold']
-        last_candle_high_price = coin_prices_data['last_candle_high_price']
-        current_candle_high_price = coin_prices_data['current_candle_high_price']
-        last_candle_low_price = coin_prices_data['last_candle_low_price']
-        current_candle_low_price = coin_prices_data['current_candle_low_price']
+def any_threshold_is_broken(trade_pair, current_candle):
+    previous_candle = Candle.objects.get(trade_pair=trade_pair)
+    thresholds = Threshold.objects.filter(trade_pair=trade_pair)
 
-        return min(last_candle_low_price, current_candle_low_price) <= threshold <= max(last_candle_high_price,
-                                                                                        current_candle_high_price)
+    for threshold in thresholds:
+        threshold_broken = threshold_is_broken(threshold.price, previous_candle, current_candle)
+        if threshold_broken:
+            return True
+
+    return False
