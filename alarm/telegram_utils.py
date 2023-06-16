7a756a7afd1c2a3d7b0334e7a1b2bac906b6ef9e
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-06-16 14:11:55 +0300

    Get TELEGRAM_BOT_TOKEN from Django settings

--- alarm/telegram_utils.py
+++ alarm/telegram_utils.py
@@ -1,17 +1,9 @@
 import asyncio
 import telegram
+from django.conf import settings
 
-bot = telegram.Bot("6182875242:AAHoyBE5VErdAyu_S2XozzcOn2Jo4180EW8")
+bot = telegram.Bot(settings.TELEGRAM_BOT_TOKEN)
 
 
 def send_message(chat_id, message):
     asyncio.run(bot.send_message(chat_id, message))
-
-def get_message_with_twiml_elements_for_threshold_break(phone, trade_pair):
-    message = TradePair(phone, trade_pair).alarm_message()
-    message_with_twiml_elements = f"<Response><Say>{message}</Say></Response>"
-    return message_with_twiml_elements
-
-
-def make_call():
-    print('MAKE CALL')
