55324982212ec0db5c8087afb6b62dcd9cca3ba4
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-06-17 16:24:04 +0300

    Update a management command to work with phone call methods

--- alarm/management/commands/price_break_threshold_alarm.py
+++ alarm/management/commands/price_break_threshold_alarm.py
@@ -24,16 +24,23 @@ class Command(BaseCommand):
 
                 binance_data = socket.recv()
                 # Placed here to be triggered first after pause caused by socket.recv()
-                Phone.stop_calling_if_call_succeed_for_each_phone()
+                Phone.sync_all_suitable_phones_alarm_messages()
 
                 high_price, low_price, close_price, trade_pair = parse_candle_from_websocket_update(binance_data)
                 Candle.refresh_candle_data(trade_pair, high_price, low_price, close_price)
 
                 created_threshold_brakes = TradePair.create_thresholds_brakes_from_recent_candles_update(trade_pair)
+
+                # Telegram logic
                 affected_phones = affected_phones.union(
                     {threshold_brake.threshold.phone for threshold_brake in created_threshold_brakes})
                 for phone in affected_phones:
                     phone.send_alarm_telegram_message()
+
+                # Phone logic
+                # Placed here to be triggered after alarm message is updated due to
+                # a previous call status sync and new candles data
+                Phone.call_or_recall_all_suitable_phones()
 
                 # TODO: recheck logic
                 # Check if new trade pair appear in the database
