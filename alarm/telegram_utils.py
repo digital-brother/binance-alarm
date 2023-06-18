2820e600998dc0076a1988815e0adc0e852f03a7
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-06-19 00:27:04 +0300

    Make send message sending a button to pause a bot. Adds python version of telegram bot.

--- alarm/telegram_utils.py
+++ alarm/telegram_utils.py
@@ -1,14 +1,17 @@
 import asyncio
 import telegram
 from django.conf import settings
-
-bot = telegram.Bot(settings.TELEGRAM_BOT_TOKEN)
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 def send_message(chat_id, message):
-    message = asyncio.run(bot.send_message(chat_id, message))
+    bot = telegram.Bot(settings.TELEGRAM_BOT_TOKEN)
+    keyboard = [[InlineKeyboardButton("Pause bot for 1 hour", callback_data='alex')]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    message = asyncio.run(bot.send_message(chat_id, message, reply_markup=reply_markup))
     return message.id
 
 
 def update_message(chat_id, message_id, new_message):
+    bot = telegram.Bot(settings.TELEGRAM_BOT_TOKEN)
     asyncio.run(bot.edit_message_text(chat_id=chat_id, message_id=message_id, text='new_message'))
