865e5829951bf76bf86dcbee2b25664b2993af38
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-06-19 01:01:55 +0300

    Clean up run_telegram_bot.py

--- alarm/management/commands/run_telegram_bot.py
+++ alarm/management/commands/run_telegram_bot.py
@@ -2,20 +2,14 @@
 For an in-depth explanation, check out
 https://github.com/python-telegram-bot/python-telegram-bot/wiki/InlineKeyboard-Example.
 """
-import logging
 from django.conf import settings
 
 from django.core.management import BaseCommand
 from telegram import Update
 from telegram.ext import Application, CallbackQueryHandler, ContextTypes
 
-logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
-# set higher logging level for httpx to avoid all GET and POST requests being logged
-logging.getLogger("httpx").setLevel(logging.WARNING)
-logger = logging.getLogger(__name__)
 
-
-async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def button_pressed_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
     await query.answer()
     await query.edit_message_text(text=query.message.text)
@@ -24,7 +18,7 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 def run_telegram_bot():
     application = Application.builder().token(settings.TELEGRAM_BOT_TOKEN).build()
-    application.add_handler(CallbackQueryHandler(button))
+    application.add_handler(CallbackQueryHandler(button_pressed_handler))
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
