b5bf5f937228058ed49917dd439042b870f54bac
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-06-25 23:07:05 +0300

    Add sentry

--- binance_alarm/settings.py
+++ binance_alarm/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import environ
 from pathlib import Path
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -154,3 +157,20 @@ TWILIO_AUTH_TOKEN = env.str('TWILIO_AUTH_TOKEN')
 TWILIO_FROM_PHONE_NUMBER = env.str('TWILIO_FROM_PHONE_NUMBER')
 
 STATIC_ROOT = 'static'
+
+SENTRY_DSN = env.str('SENTRY_DSN')
+sentry_sdk.init(
+    dsn=SENTRY_DSN,
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
