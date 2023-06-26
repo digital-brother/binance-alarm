8bbd9a8050bb94c7f771c92b7996070b073bfede
Oleksandr Shuryhin <shuryhin.oleksandr@gmail.com>
2023-06-26 13:31:55 +0300

    Add access rights to admin thresholds, phones foreign keys

--- alarm/management/commands/run_telegram_bot.py
+++ alarm/management/commands/run_telegram_bot.py
@@ -14,9 +14,15 @@ async def alarm_message_seen_handler(update: Update, context: ContextTypes.DEFAU
     await Phone.objects.filter(telegram_chat_id=update.message.chat.id).aupdate(telegram_message_seen=True)
 
 
+async def get_chat_id_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.message.chat_id
+    context.bot.send_message(chat_id=chat_id, text=f"Your chat ID is {chat_id}")
+
+
 def run_telegram_bot():
     application = Application.builder().token(settings.TELEGRAM_BOT_TOKEN).build()
     application.add_handler(CommandHandler("alarm_message_seen", alarm_message_seen_handler))
+    application.add_handler(CommandHandler("get_chat_id", get_chat_id_handler))
     application.run_polling(allowed_updates=Update.CALLBACK_QUERY)
 
 
